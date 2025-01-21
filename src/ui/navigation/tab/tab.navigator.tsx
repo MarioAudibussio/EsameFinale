f242fbb907b362ce2deba8006bccc3dcc477bde3
MarioAudibussio <mario.audibussio@edu.itspiemonte.it>
2025-01-21 15:09:10 +0100

    new features: add tab bar

---
+++ src/ui/navigation/tab/tab.navigator.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TabParams, Screen } from '../types';
+import HomeScreen from '../../screens/home/home.screen';
+import { Ionicons } from '@expo/vector-icons';
+import FavoritesScreen from '../../screens/notifications/favoritesScreen';
+
+const Tab = createBottomTabNavigator<TabParams>();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => {
+        return {
+          headerShown: true,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            backgroundColor: '#001945',
+            borderTopColor: '#333333',
+          },
+          headerStyle: {
+            backgroundColor: '#1a1a1a',
+          },
+          headerTintColor: '#ffffff',
+          tabBarIconStyle: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          },
+          tabBarIcon: ({ focused }) => {
+            const iconName = () => {
+              switch (route.name) {
+                case Screen.Home:
+                  return 'home';
+                case Screen.Favorites:
+                  return 'heart';
+              }
+            };
+
+            return (
+              <Ionicons 
+                name={iconName()} 
+                size={24} 
+                color={focused ? '#3579f6' : '#38B6FF'}
+              />
+            );
+          },
+        };
+      }}>
+      <Tab.Screen name={Screen.Home} component={HomeScreen}initialParams={{ hasFavoritesUpdated: false }}/>
+      <Tab.Screen name={Screen.Favorites} component={FavoritesScreen}initialParams={{ hasFavoritesUpdated: false }}/>
+    </Tab.Navigator>
+  );
+}
